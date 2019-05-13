da14f6e6417abcccfdf04dedc459514cb5b71b85
Dan Mincu <danmincu@gmail.com>
2019-05-13 12:50:30 -0400

    experiments services and logo

---
+++ Experiments/KillProcess/KillProcess/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Management;
+
+namespace KillProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ProcessName = args[0];
+            string ProcessUserName = args[1];
+            string HasStartedForHours = args[2];
+            ProcessHelper.ProcessHelper ph = new ProcessHelper.ProcessHelper();
+
+            ph.KillProcessByNameAndUser(ProcessName, ProcessUserName, Convert.ToInt32(HasStartedForHours));
+           
+            Console.Read();
+        }
+
+
+
+
+    }
+}
