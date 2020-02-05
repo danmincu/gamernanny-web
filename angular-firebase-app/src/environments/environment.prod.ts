93cb1a1a559fe6f96bd333b8a24d0a32b4a33f67
Dan Mincu <danmincu@gmail.com>
2020-02-04 22:39:29 -0500

    scaffolding Angular firebase app

---
+++ angular-firebase-app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC27VKiSct2xObenxZkOdh8e0IjRydFxIM",
+    authDomain: "gamer-nanny.firebaseapp.com",
+    databaseURL: "https://gamer-nanny.firebaseio.com",
+    storageBucket: "gamer-nanny.appspot.com",
+    projectId: "gamer-nanny",    
+    messagingSenderId: "702787418681"
+  }
+};
