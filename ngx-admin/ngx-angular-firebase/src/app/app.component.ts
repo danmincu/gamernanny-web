0873085d0477fb67ae899e14ce097ad8207ce350
Dan Mincu <danmincu@gmail.com>
2020-02-13 21:06:07 -0500

    Better approach to the loading nanny cart when visiting the website

--- ngx-admin/ngx-angular-firebase/src/app/app.component.ts
+++ ngx-admin/ngx-angular-firebase/src/app/app.component.ts
@@ -6,18 +6,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SeoService } from './@core/utils/seo.service';
+import { delay } from 'rxjs/operators';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
+  doneLoading = new BehaviorSubject<boolean>(false);
   constructor(private analytics: AnalyticsService, private seoService: SeoService) {
   }
 
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
+    setTimeout(() => {this.doneLoading.next(true)}, 6000);
   }
 }
